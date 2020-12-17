91843becfebaa6e5d2f74a3871c7597b325e1ef0
Byeongju Yu <yoobj17@gmail.com>
2020-12-18 02:24:44 +0900

    practice day1

---
+++ Base_Swift/LearnSwift.playground/Pages/Hello Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+/*:
+# Welcome to Plaugrounds
+ This is your *first* playground which is intended to demonstrate:
+ * The use of **Quick Look**
+ */
+import UIKit
+
+var str = "Hello, playground"
+print("Welcome to Swift")
+print(str)
+
+var x = 10
+
+for index in 1...20 {
+    let y = index * x
+    x -= 1
+    print(y)
+}
