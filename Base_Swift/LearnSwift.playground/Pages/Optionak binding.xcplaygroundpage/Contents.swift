62b7a2f3c112ce4b1e8722a226dfae9afba84419
Byeongju Yu <yoobj17@gmail.com>
2021-01-28 23:53:38 +0900

    create MusicPlayer

--- Base_Swift/LearnSwift.playground/Pages/Optionak binding.xcplaygroundpage/Contents.swift
+++ Base_Swift/LearnSwift.playground/Pages/Optionak binding.xcplaygroundpage/Contents.swift
@@ -1,11 +1,1 @@
 
-var index: Int?
-index = 3
-
-var treeArray = ["Oak", "Pine", "Yew", "Birch"]
-
-if let myValue = index {
-    print(treeArray[myValue])
-} else {
-    print("index does not contain a value")
-}
---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 유병주 on 2021/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
