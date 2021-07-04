15cee11f2b51576097e4cb54f2d36932b0437665
ByeongjooYu <yoobj17@gmail.com>
2021-07-05 01:49:39 +0900

    solve step8 problem 1,2

--- Swift_practice/Swift_practice/BaekJoon Algorithm/Step_8/1_1712.swift
+++ Swift_practice/Swift_practice/BaekJoon Algorithm/Step_8/1_1712.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+func calculate_BEP(_ fixedCost: Int, _ variableCost: Int, _ price: Int) -> Int{
+    if variableCost >= price{
+        return -1
+    }else{
+        return fixedCost / (price - variableCost) + 1
+    }
+}
+
+let info: [Int] = readLine()!.components(separatedBy: " ").map{ Int($0)!}
+
+print(calculate_BEP(info[0], info[1], info[2]))
--- Swift_practice/Swift_practice/BaekJoon Algorithm/Step_8/2_2292.swift
+++ Swift_practice/Swift_practice/BaekJoon Algorithm/Step_8/2_2292.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+import Foundation
+
+var num: Int = Int(readLine()!)!
+var result: Int = 0
+var sum: Int = 0
+while sum < num {
+    if result == 0{
+        sum = 1
+    }else{
+        sum += 6 * result
+    }
+    result += 1
+}
+print(result)
