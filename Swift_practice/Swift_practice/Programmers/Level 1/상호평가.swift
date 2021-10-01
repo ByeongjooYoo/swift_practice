07919cf7b3e84bc348ab9c0d9bf745aa91a0bcf4
ByeongjooYoo <yoobj17@gmail.com>
2021-10-01 17:48:08 +0900

    solve programmers level1 "상호평가"

--- Swift_practice/Swift_practice/Programmers/Level 1/상호평가.swift
+++ Swift_practice/Swift_practice/Programmers/Level 1/상호평가.swift
@@ -1,10 +1,9 @@
 //
-//  main.swift
+//  상호평가.swift
 //  Swift_practice
 //
-//  Created by 유병주 on 2021/04/19.
+//  Created by ByeongJu Yu on 2021/10/01.
 //
-
 
 import Foundation
 
@@ -58,5 +57,3 @@ func calculateGrade(_ averageScore: Double) -> String {
         return "F"
     }
 }
-
-print(solution([[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,80,65],[24,90,94,75,65]]))
