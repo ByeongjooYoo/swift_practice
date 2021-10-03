28b4dc69c2417558d1e4d9e21645d34df00eaec9
ByeongjooYoo <yoobj17@gmail.com>
2021-10-03 14:15:04 +0900

    solve programmers level1 "예산"

--- Swift_practice/Swift_practice/Programmers/Level 1/예산.swift
+++ Swift_practice/Swift_practice/Programmers/Level 1/예산.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+func solution(_ d:[Int], _ budget:Int) -> Int {
+    let sortD = d.sorted()
+    var count = 0
+    var budgetSum = 0
+    for i in sortD {
+        if budgetSum + i <= budget {
+            budgetSum += i
+            count += 1
+        } else {
+            break
+        }
+    }
+    return count
+}
