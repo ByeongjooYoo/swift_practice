d3c65fb7ee9610cfe5bbd2bbac8e7daef4542fbb
ByeongjooYoo <yoobj17@gmail.com>
2021-10-05 13:52:52 +0900

    solve programmers level1 "체육복"

--- Swift_practice/Swift_practice/Programmers/Level 1/체육복.swift
+++ Swift_practice/Swift_practice/Programmers/Level 1/체육복.swift
@@ -1,24 +1,23 @@
 //
-//  main.swift
+//  체육복.swift
 //  Swift_practice
 //
-//  Created by 유병주 on 2021/04/19.
+//  Created by ByeongJu Yu on 2021/10/05.
 //
-
 
 import Foundation
 
 func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
-    var lostMan = lost
+    var lostMan = lost.sorted()
     var reserve1 = reserve.sorted()
-    for i in lost.sorted() {
+    for i in lost {
         if reserve1.contains(i) {
             lostMan.remove(at: lostMan.firstIndex(of: i)!)
             reserve1.remove(at: reserve1.firstIndex(of: i)!)
         }
     }
     var lostResult = lostMan
-    for i in lostMan.sorted() {
+    for i in lostMan {
         for j in reserve1 {
             if i - 1 == j || i + 1 == j {
                 lostResult.remove(at: lostResult.firstIndex(of: i)!)
@@ -31,6 +30,3 @@ func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
     let result = n - lostResult.count
     return result
 }
-
-print(solution(5, [1,2,3], [2,3,4]))
-
