e2ce68c94b1a798dee3e6dd8cf41e16d3243e40d
ByeongjooYoo <yoobj17@gmail.com>
2021-10-06 11:44:28 +0900

    solve programmers level1 "음양 더하기"

---
+++ Swift_practice/Swift_practice/Programmers/Level 1/음양 더하기.swift
@@ -0,0 +1,20 @@
+//
+//  음양 더하기.swift
+//  Swift_practice
+//
+//  Created by ByeongJu Yu on 2021/10/06.
+//
+
+import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var result = 0
+    for i in 0 ..< absolutes.count {
+        if signs[i] {
+            result += absolutes[i]
+        } else {
+            result += -absolutes[i]
+        }
+    }
+    return result
+}
