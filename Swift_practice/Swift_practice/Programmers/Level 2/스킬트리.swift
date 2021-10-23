5c3c2bcd7270da4078490d9da3bd5f5e583ccaa6
ByeongjooYoo <yoobj17@gmail.com>
2021-10-23 15:51:01 +0900

    solve programmers level2 "스킬트리"

--- Swift_practice/Swift_practice/Programmers/Level 2/스킬트리.swift
+++ Swift_practice/Swift_practice/Programmers/Level 2/스킬트리.swift
@@ -1,8 +1,8 @@
 //
-//  main.swift
+//  스킬트리.swift
 //  Swift_practice
 //
-//  Created by 유병주 on 2021/04/19.
+//  Created by ByeongJu Yu on 2021/10/23.
 //
 
 import Foundation
@@ -46,26 +46,3 @@ func solution(_ skill:String, _ skill_trees:[String]) -> Int {
     }
     return count
 }
-
-let test1 = ("CBD", ["CAD"]) // 0
-let test2 =  ("CBD", ["AEF", "ZJW"]) //2
-let test3 =  ("REA", ["REA", "POA"]) //1
-let test4 = ("CBDK", ["CB", "CXYB", "BD", "AECD", "ABC", "AEX", "CDB", "CBKD", "IJCB", "LMDK"]) //4
-let test5 = ("BDC", ["AAAABACA"]) //0
-let test6 = ("CBD", ["C", "D", "CB", "BDA"]) //2
-
-
-print(solution(test1.0, test1.1))
-
-print(solution(test2.0, test2.1))
-
-print(solution(test3.0, test3.1))
-
-print(solution(test4.0, test4.1))
-
-print(solution(test5.0, test5.1))
-
-print(solution(test6.0, test6.1))
-
-
-
