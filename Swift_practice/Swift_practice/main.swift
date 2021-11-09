b238cf14a2a24e0fa870220323eb26ce1f1e7c32
ByeongjooYoo <yoobj17@gmail.com>
2021-11-09 13:57:24 +0900

    remove test file

--- Swift_practice/Swift_practice/main.swift
+++ Swift_practice/Swift_practice/main.swift
@@ -7,6 +7,34 @@
 
 import Foundation
 
-var string = ["hello", "one", "even", "never", "now", "world", "draw"]
-var str = "hello"
-print(string.first!)
+func solution(_ s:String) -> [Int] {
+    var result: [Int] = []
+    var count = 0
+    let array = Array(s).map { String($0) }
+    for i in 0 ..< array.count {
+        if i == 0 {
+            count += 1
+            continue
+        }
+        if array[i] == array[i - 1] {
+            count += 1
+        } else {
+            result.append(count)
+            count = 1
+        }
+        if i == array.count - 1 {
+            result.append(count)
+        }
+    }
+    if array[0] == array[array.count - 1] {
+        let num = result.first! + result.last!
+        print(num)
+        result.removeFirst()
+        result.removeLast()
+        result.append(num)
+    }
+    return result.sorted()
+}
+
+
+print(solution("aabbcddaada"))
